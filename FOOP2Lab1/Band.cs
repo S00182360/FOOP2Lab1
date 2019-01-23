71da2fe20c36b278cffe27b738331ed93ef3011e
Samuel Colville <S00182360@mail.itsligo.ie>
2019-01-23 14:57:21 +0000

    - WIndow created - Band class and 6 band objects created - IComparable sort by Band Name - BandList sorted and displays in lbxBandList

---
+++ FOOP2Lab1/Band.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOOP2Lab1
+{
+    class Band : IComparable
+    {
+        public string BandName { get; set; }
+        public int Year { get; set; }
+        public List<string> Members { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Band that = (Band)obj;
+            return BandName.CompareTo(that.BandName);
+        }
+
+        public override string ToString()
+        {
+            return BandName;
+        }
+    }
+}
--- FOOP2Lab1/MainWindow.xaml.cs
+++ FOOP2Lab1/MainWindow.xaml.cs
@@ -15,14 +15,63 @@ using System.Windows.Shapes;
 
 namespace FOOP2Lab1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        List<Band> BandList = new List<Band>();
+
         public MainWindow()
         {
             InitializeComponent();
+            //Set bands
+            Band b1 = new Band
+            {
+                BandName = "Muse",
+                Year = 1994,
+                Members = new List<string> { "Matthew Belamy", "Chris Wolstenholme", "Dominic Howard" }
+            };
+            Band b2 = new Band
+            {
+                BandName = "Panic at the Disco",
+                Year = 2004,
+                Members = new List<string> { "Brendon Urie", "Ryan Ross", "Dallon Weekes",
+                    "Jon Walker", "Spencer Smith", "Brent Wilson" }
+            };
+            Band b3 = new Band
+            {
+                BandName = "The Beach Boys",
+                Year = 1961,
+                Members = new List<string> { "Brian Wilson", "Mike Love", "Dennis Wilson", "Carl Wilson"}
+            };
+            Band b4 = new Band
+            {
+                BandName = "One Direction",
+                Year = 2010,
+                Members = new List<string> { "Niall Horan", "Liam Payne", "Harry Styles",
+                    "Louis Tomlinson", "Zayne Malik" }
+            };
+            Band b5 = new Band
+            {
+                BandName = "Artic Monkeys",
+                Year = 2002,
+                Members = new List<string> {  "Alex Turner", "Matt Helders", "Jamie Cook",
+                    "Nick O'Malley", "Andy Nicholson", "Glyn Jones" }
+            };
+            Band b6 = new Band
+            {
+                BandName = "The The",
+                Year = 1979,
+                Members = new List<string> { "Matt Johnson", "Johnny Marr", "Jools Holland" }
+            };
+            //Add to bandlist
+            BandList.Add(b1);
+            BandList.Add(b2);
+            BandList.Add(b3);
+            BandList.Add(b4);
+            BandList.Add(b5);
+            BandList.Add(b6);
+            BandList.Sort();
+            BandList.Reverse();
+            lbxBandList.ItemsSource = BandList;
         }
     }
 }
